f36b90cd3c962288c85a924fd3f6cc76252ec70e
anthonypeters <apeters10@luc.edu>
2021-01-21 13:42:51 -0600

    Got it mostly working
    Got it working through insomnia client, for some reason tradinview won't send post request to aws URL. Or rather, it's not sending the Bracket order to Alpaca from tradingview.

---
+++ app.py
@@ -0,0 +1,63 @@
+import requests, json
+from chalice import Chalice
+
+app = Chalice(app_name='tradingview-webhooks')
+
+
+API_KEY = "PKIHF26ARGET0YRSFSHB"
+SECRET_KEY = "n3qvT6B6N3e4zMIWG8gr6pPX740QsbOQsN06togP"
+BASE_URL = "https://paper-api.alpaca.markets"
+ORDERS_URL = "{}/v2/orders".format(BASE_URL)
+HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
+    
+@app.route('/buy_stock', methods=['POST'])
+def buy_stock():
+    request = app.current_request
+    webhook_message = request.json_body
+
+    data = {
+        "symbol": webhook_message['ticker'],
+        "qty": 1,
+        "side": "buy",
+        "type": "limit",
+        "limit_price": webhook_message['close'],
+        "time_in_force": "gtc",
+        "order_class": "bracket",
+        "take_profit": {
+            "limit_price": webhook_message['close'] * 1.05
+        },
+        "stop_loss": {
+            "stop_price": webhook_message['close'] * 0.98,
+        }
+    }
+    
+    r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
+    response = json.loads(r.content)
+
+    return {
+        'message': 'I bought the stock!',
+        'webhook_message': webhook_message,
+        'id': response['id'],
+        'client_order_id': response['client_order_id']
+    }
+
+
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
